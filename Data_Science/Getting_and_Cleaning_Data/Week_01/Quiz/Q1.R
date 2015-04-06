75af0d4b381c6cb8cf6981b7efaf8e98c8e7b540
Alexander Kim <alexander.kim.email@gmail.com>
2015-04-06 14:05:10 -0700

    Files for Quiestion 1

---
+++ Data_Science/Getting_and_Cleaning_Data/Week_01/Quiz/Q1.R
@@ -0,0 +1,15 @@
+# The American Community Survey distributes downloadable data about United States communities. Download the 2006 microdata survey about housing for the state of Idaho using download.file() from here: 
+#   
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv 
+# 
+# and load the data into R. The code book, describing the variable names is here: 
+#   
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+# 
+# How many properties are worth $1,000,000 or more?
+
+housingdata <- read.csv("getdata-data-ss06hid.csv")
+data <- housingdata[housingdata$VAL == "24",]$VAL
+print(length(data[!is.na(data)]))
+
+
