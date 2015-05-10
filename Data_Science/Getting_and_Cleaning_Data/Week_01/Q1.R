4cbf941f991692a73cb5c1451d8cb8ea507a83d1
Alexander Kim <alexander.kim.email@gmail.com>
2015-05-09 18:46:24 -0700

    minot edit

--- Data_Science/Getting_and_Cleaning_Data/Week_01/Q1.R
+++ Data_Science/Getting_and_Cleaning_Data/Week_01/Q1.R
@@ -7,7 +7,8 @@
 #   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
 # 
 # How many properties are worth $1,000,000 or more?
-
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
 housingdata <- read.csv("getdata-data-ss06hid.csv")
 data <- housingdata[housingdata$VAL == "24",]$VAL
 print(length(data[!is.na(data)]))
